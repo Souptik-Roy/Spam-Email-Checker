07dae11ccd206d5a51ef5a98f9f5bb52d2f6411e
Souptik Roy <155081347+Souptik-Roy@users.noreply.github.com>
2025-08-21 01:24:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 
 # Load pipeline
-with open("models/spam-checker-pipeline.pkl", "rb") as f:
+with open("spam-checker-pipeline.pkl", "rb") as f:
     spam_clf = pickle.load(f)
 
 st.title("📧 Spam Email/SMS Classifier")
@@ -11,4 +11,5 @@ message = st.text_area("Enter a message:")
 if st.button("Check"):
     prediction = spam_clf.predict([message])[0]
     st.write("### Prediction: ", "🚨 Spam" if prediction == 1 else "✅ Not Spam")
+
 
