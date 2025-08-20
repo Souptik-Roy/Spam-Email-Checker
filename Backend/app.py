7cd400f07d1348d5240be22bab58ecc0bed77a89
Souptik Roy <155081347+Souptik-Roy@users.noreply.github.com>
2025-08-21 01:03:39 +0530

    Add files via upload

---
+++ Backend/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pickle
+
+# Load pipeline
+with open("spam-checker-pipeline.pkl", "rb") as f:
+    spam_clf = pickle.load(f)
+
+st.title("📧 Spam Email/SMS Classifier")
+
+message = st.text_area("Enter a message:")
+if st.button("Check"):
+    prediction = spam_clf.predict([message])[0]
+    st.write("### Prediction: ", "🚨 Spam" if prediction == 1 else "✅ Not Spam")
